7d8a964055aac256213156975ed96312abc8ad0a
nos <nostradamus411@gmail.com>
2021-07-19 01:24:26 -0500

    votes hub1

--- cosmo_gov.py
+++ cosmo_gov.py
@@ -3,74 +3,44 @@ import json
 import subprocess
 
 def main():
-    max_num = 37
-    ion_cosmos_gov={}
-    api = os.environ['ATOM_API']
-    # ion_drop_dict = {"cosmos1x5wgh6vwye60wv3dtshs9dmqggwfx2ldnqvev0": 42}
+    hub=1
+    hub1_props = range(1,6)
+    hub1_votes_dict = {}
     ion_drop_dict = open_drop_file()
 
-    # Loop through all the addresses and pull gov activity through proposal 37
-    try:
-        for atom_addr in ion_drop_dict:
-            prop_num = 1
-            if ion_cosmos_gov is None:
-                ion_cosmos_gov = {}
-            ions = {"ions":ion_drop_dict[atom_addr]}
-            ion_cosmos_gov[atom_addr] = ions
-            while prop_num <= max_num:
-                this_prop = {}
-                propose = proposal(prop_num,atom_addr,api)
-                this_prop.update(propose)
-                dep_amt = deposit(prop_num,atom_addr,api)
-                this_prop.update(dep_amt)
-                voted = vote(prop_num,atom_addr,api)
-                this_prop.update(voted)
-                ion_cosmos_gov[atom_addr][f"prop{prop_num}"] = this_prop
-                prop_num += 1
-        write(ion_cosmos_gov)
-    except:
-        write(ion_cosmos_gov)
+    for atom_addr in ion_drop_dict:
+        if hub1_votes_dict is None:
+            hub1_votes_dict = {}
+        ions = {"ions":ion_drop_dict[atom_addr]}
+        hub1_votes_dict[atom_addr] = ions        
+        for prop in hub1_props:
+            this_prop = {}
+            hub1_vote_ret = hub1_vote(prop,atom_addr)
+            vote = {'vote':hub1_vote_ret['option']}
+            this_prop.update(vote)
+            hub1_votes_dict[atom_addr][f"prop{prop}"] = this_prop
 
-def write(dict):
-    with open('ion_cosmos_gov.json', 'x') as gov:
-        json.dump(dict,gov)  
+    frmt_out = json.dumps(hub1_votes_dict, indent=4)
+    write_json(frmt_out,hub)
 
-def vote(num,addr,api):
-    cmd = f'curl https://stargate-final--lcd--archive.datahub.figment.io/apikey/{api}/gov/proposals/{num}/votes/{addr}'
-    output = subprocess.check_output(cmd, shell=True)
-    output_dict = json.loads(output.decode('utf-8'))
-    try:
-        ret = {"voted":output_dict['result']['option']}
-    except:
-        ret = {"voted":{}}
-    return ret
+# Output txt file of the total amount of unclaimed ions
+def write_json(dict,hub):
+    with open(f'hub{hub}_votes_dict.json', 'w') as out:
+        out.write(dict)
 
-def deposit(num,addr,api):
-    cmd = f'curl https://stargate-final--lcd--archive.datahub.figment.io/apikey/{api}/gov/proposals/{num}/deposits/{addr}'
-    output = subprocess.check_output(cmd, shell=True)
-    output_dict = json.loads(output.decode('utf-8'))
+def hub1_vote(prop,addr):
+    cmd = f'~/cosmos/hub1/gaiacli query gov vote {prop} {addr} -o json  --trust-node'
     try:
-        ret = {"deposited":output_dict['result']['amount']}
-    except:
-        ret = {"deposited":{}}
-    return ret
-
-def proposal(num,addr,api):
-    cmd = f'curl https://stargate-final--lcd--archive.datahub.figment.io/apikey/{api}/gov/proposals/{num}/proposer'
-    output = subprocess.check_output(cmd, shell=True)
-    output_dict = json.loads(output.decode('utf-8'))
-    try:
-        if output_dict['result']['proposer'] == addr:
-            ret = {"proposer":True}
-        else:
-            ret = {"proposer":False}
-    except:
-        ret = {"proposer":False}
-    return ret
+        output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+        output_dict = json.loads(output.decode('utf-8'))
+        ret = output_dict.pop('voter')
+        return output_dict
+    except subprocess.CalledProcessError as e:
+        return {'proposal_id': prop, 'option': 'DidNotVote'}
 
 # Open ion airdrop json file
 def open_drop_file():
-    with open("ions.json","r") as file:
+    with open("ion_test.json","r") as file:
         file_py_obj = json.load(file)      
     return file_py_obj
 
